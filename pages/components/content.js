cf2a83ce2aeaf7b03588fac968475afc6960104c
kanoi-y <furafuratonbo@gmail.com>
2021-09-23 01:08:30 +0900

    リポジトリを作成、swrをインストール

---
+++ pages/components/content.js
@@ -0,0 +1,9 @@
+export default function Content({ children }) {
+  return (
+    <>
+      <div className="container">{children}</div>
+      <hr />
+      <footer>©︎ 2021 ともすた</footer>
+    </>
+  );
+}
